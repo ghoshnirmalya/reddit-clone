0653c7477298aa4a2b341397b28904f03abacfc6
Nirmalya Ghosh <nirmalya.email@gmail.com>
2017-06-02 10:20:49 +0530

    feat(app): bootstrap a crea-react-app project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import registerServiceWorker from './utils/registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();

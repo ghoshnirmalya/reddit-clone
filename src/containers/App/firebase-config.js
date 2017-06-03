2c09fc56e503027f5c4a98306816752cee18b48c
Nirmalya Ghosh <nirmalya.email@gmail.com>
2017-06-03 19:23:11 +0530

    feat(app): add firebase

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBRExKF0cHylh_wFLcd8Vxugj0UQRpq8oc",
+  authDomain: "reddit-clone-53da5.firebaseapp.com",
+  databaseURL: "https://reddit-clone-53da5.firebaseio.com",
+  projectId: "reddit-clone-53da5",
+  storageBucket: "reddit-clone-53da5.appspot.com",
+  messagingSenderId: "490290211297"
+};
